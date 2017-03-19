3cfe8f464986cc9269530a5a6967e2d5eb50ef79
Anuar Bolatov <Anuar Bolatov>
2017-03-19 01:36:55 +0000

    added quotation mark and stated working on footer

--- app/js/modules/scroll-based-animation.js
+++ app/js/modules/scroll-based-animation.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
   var $animation_elements = $('.animation-element');
+  var $quote_mark = $('.quote-icon');
   var $window = $(window);
 
   function check_if_in_view() {
@@ -24,6 +25,24 @@ $(document).ready(function(){
       }
 
     });
+
+    $.each($quote_mark, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+
+      (element_top_position <= window_bottom_position)) {
+
+      $element.addClass('in-view-rotate');
+
+      }
+
+    });
+
   }
 
   $window.on('scroll resize', check_if_in_view);
